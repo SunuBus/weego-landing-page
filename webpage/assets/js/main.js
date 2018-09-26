74a5025c1efe50d2f4a2c923539821559654ad01
Saalihou Ndiaye <saalihou@gmail.com>
2018-09-26 13:47:17 +0000

    Change countdown to connecting morocco message

--- webpage/assets/js/main.js
+++ webpage/assets/js/main.js
@@ -70,13 +70,13 @@ console.log('%c Proudly Crafted with ZiOn.', 'background: #222; color: #bada55')
 
 
 //Provide the plugin settings
-$("#countdown").countdown({
-    //The countdown end date
-    date: "01 September 2018 12:00:00",
+// $("#countdown").countdown({
+//     //The countdown end date
+//     date: "01 September 2018 12:00:00",
     
-    // on (03:07:52) | off (3:7:52) - two_digits set to ON maintains layout consistency
-    format: "on"
-}, 
+//     // on (03:07:52) | off (3:7:52) - two_digits set to ON maintains layout consistency
+//     format: "on"
+// }, 
 
 function() {
 // This will run when the countdown ends
